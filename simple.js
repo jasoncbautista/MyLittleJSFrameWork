9f0b06917929abc18d2c9d712de832d47fbc37b9
Jason <bjason@gmail.com>
2014-09-30 00:56:42 -0700

    Maps,

--- simple.js
+++ simple.js
@@ -1,6 +1,8 @@
 var root = this;
 
 (function(){
+    var J = function(){};
+    root.J = J;
     root.J$= {
         Core: {}
     };
@@ -11,5 +13,39 @@ var root = this;
     root.J$.Running.Entities = {};
 
     // Watch values and update them
+    
 
-});
+
+    // My own little jQuery, mootools
+    root.JQ = function(){
+    };
+
+    
+    J.map= function(list, callback){
+        if(Array.isArray(list)){
+            
+            // Loop over array
+            var len = list.length;
+            for(var ii = 0; ii < len; ii++){
+                callback(list[ii]);
+            }
+        } else{
+
+            // Loop over hashMap:
+            for (prop in list){
+                if(Object.prototype.hasOwnProperty.call(list, xx){
+                    callback(xx);
+                }
+            }
+
+        }
+
+    };
+
+
+    // A simple null function, does nothing
+    J.noop = function(){};
+
+    // document.querySelectorAll("#templates")[0].innerHTML
+
+})(root);
