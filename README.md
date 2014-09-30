7846674fd3274bfbeeec27cc733e3da8a5798abb
Jason <bjason@gmail.com>
2014-09-30 00:25:11 -0700

    Core

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MyLittleJSFrameWork
 
+
+# Goal:
+
+No jQuery
--- simple.js
+++ simple.js
@@ -2,9 +2,16 @@ var root = this;
 
 (function(){
     root.JAZON = {
-        Core = {}
+        Core: {}
     };
 
     console.log('root', root);
+
+
+    root.Running = {};
+    root.Running.Entities = {};
+
+    // Watch values and update them
+
 
 });
